51ebe53f88134b3dd3f783088ac5a184575b0771
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-12-30 17:21:30 +0000

    implement custom validators with auto error with default error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { RxFormGroup, RxFormBuilder } from "@rxweb/reactive-form-validators";
 import Register from "./models/resgister";
 import { TranslateService } from "@ngx-translate/core";
+import { TextValidator } from "./validations-module/validators/text.validator";
+import { FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -23,9 +25,20 @@ export class AppComponent implements OnInit {
     this.registerFormGroup = this.formBuilder.formGroup(
       this.register
     ) as RxFormGroup;
+
+    const emailControl = this.registerFormGroup.controls.email as FormControl;
+    const emailValidator = emailControl.validator;
+    emailControl.setValidators([
+      TextValidator.regexMatch(/\S+@\S+\.\S+/, "VALIDATIONS.EMAIL_FORMAT"),
+      TextValidator.hasWords(
+        ["example", "test", "123"],
+        "VALIDATIONS.EMAIL_FORBIDDEN_WORDS"
+      ),
+      emailValidator
+    ]);
   }
 
-  public onLanguageChange({target}): void {
+  public onLanguageChange({ target }): void {
     this.translateService.use(target.value);
   }
 
@@ -35,6 +48,19 @@ export class AppComponent implements OnInit {
 
   public getCurrentLanguage(): string {
     return this.translateService.currentLang;
+  }
+
+  public onSubmit(): void {
+    // Show default error with alert message
+    const emailFormControl = this.registerFormGroup.controls.email;
+    if (emailFormControl.errors && emailFormControl.errors.hasWordsError) {
+      const error = emailFormControl.errors.hasWordsError;
+      alert(
+        this.translateService.instant(error.errorTranslation, {
+          values: error.values
+        })
+      );
+    }
   }
 
   private setupTranslations() {
--- src/app/validations-module/validators/text.validator.ts
+++ src/app/validations-module/validators/text.validator.ts
@@ -2,6 +2,7 @@ import * as _ from "lodash";
 
 import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 import { AutoError } from "../models/auto-error.model";
+import { DefaultError } from "../models/default-error.model";
 
 export class TextValidator {
   public static regexMatch(
@@ -11,7 +12,23 @@ export class TextValidator {
     return (control: AbstractControl): ValidationErrors | null => {
       if (!_.isEmpty(control.value) && !regExp.test(control.value)) {
         return {
-          regexPattern: new AutoError(translationKey, { values: { regExp } })
+          regexPattern: new AutoError("", translationKey, {
+            values: { regExp }
+          })
+        };
+      }
+      return null;
+    };
+  }
+
+  public static hasWords(words: string[], translationKey: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (_.some(words, word => _.includes(control.value, word))) {
+        return {
+          hasWordsError: new DefaultError({
+            errorTranslation: translationKey,
+            values: words.join(", ")
+          })
         };
       }
       return null;
